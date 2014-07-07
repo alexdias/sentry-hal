9e7937f3a017b930c4d69839cb7c418366d40bec
Alexandre Dias <alex.dias@smarkets.com>
2014-07-07 11:34:30 +0100

    Fix various bugs

--- sentry_hal/plugin.py
+++ sentry_hal/plugin.py
@@ -37,10 +37,14 @@ class HALMessage(Plugin):
         message = '[sentry %s] %s (%s)' % (event.server_name, event.message, link)
         self.send_payload(event.project, message)
 
+    def resolve_address_host(self, address):
+        assert len(address) == 2, address
+        return (socket.gethostbyname(address[0]), address[1])
+
     def send_payload(self, project, message):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.connect((self.get_option('host', project), self.get_option('port', project)))
-        msg = json.dumps(dict(message=message, room=self.get_option('channel', project))).encode()
-        bytes_sent = sock.sendto(payload, self._address)
+        address = self.resolve_address_host((self.get_option('host', project), int(self.get_option('port', project))))
+        payload = json.dumps(dict(message=message, room=self.get_option('channel', project))).encode()
+        bytes_sent = sock.sendto(payload, address)
         if bytes_sent != len(payload):
             log.warn('Sent %s bytes instead of %s. Payload: %s', bytes_sent, len(payload), payload)
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = [
 
 setup(
     name='sentry-hal',
-    version='0.1.1',
+    version='0.1.2',
     author='Alexandre Dias',
     author_email='alexnvdias@gmail.com',
     url='http://www.github.com/alexnvdias',
